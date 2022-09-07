f6dc252697e4f457d71563a92408f056c97774d8
Mihai <25897491+mehiX@users.noreply.github.com>
2022-09-07 05:54:07 +0200

    start with user service

--- internal/app/model.go
+++ internal/app/model.go
@@ -1,5 +1,13 @@
 package app
 
+import "errors"
+
+var (
+	ErrNotImplemented  = errors.New("not implemented")
+	ErrUsernameExists  = errors.New("user already exists")
+	ErrInvalidPassword = errors.New("password is invalid")
+)
+
 type Product struct {
 	AmountAvailable int64  `json:"amount_available"`
 	Cost            int64  `json:"cost"`
--- internal/app/service_user.go
+++ internal/app/service_user.go
@@ -1,1 +1,64 @@
 package app
+
+import (
+	"log"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var users []User
+
+func NewUser() User {
+	return User{
+		ID: uuid.New().String(),
+	}
+}
+
+func (a *app) UserAdd(username, password string, role TypeRole) (string, error) {
+	if userExists(username) {
+		return "", ErrUsernameExists
+	}
+
+	if !validPassword(password) {
+		return "", ErrInvalidPassword
+	}
+
+	usr := NewUser()
+	usr.Username = username
+	pwd, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+	if err != nil {
+		return "", err
+	}
+	usr.Password = string(pwd)
+	usr.Role = role
+
+	users = append(users, usr)
+
+	return usr.ID, nil
+}
+
+func userExists(username string) bool {
+	for _, u := range users {
+		if u.Username == username {
+			return true
+		}
+	}
+
+	return false
+}
+
+func validPassword(p string) bool {
+	return len(p) >= 8
+}
+
+func comparePasswords(hashedPwd string, plainPwd []byte) bool {
+	byteHash := []byte(hashedPwd)
+	err := bcrypt.CompareHashAndPassword(byteHash, plainPwd)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	return true
+}
